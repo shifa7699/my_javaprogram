adf6af4533c338f639633719bea79ba7d9367ea1
shifa7699 <111556272+shifa7699@users.noreply.github.com>
2022-08-19 13:32:09 +0530

    Update equal.java

--- equal.java
+++ equal.java
@@ -7,11 +7,11 @@ public class equal
 		int a=6,b=6,c=6,d=6;
 		if(a==b && b==c && c==d)
 		{
-			Systtem.out.println("All the integers are equal");
+			System.out.println("All the integers are equal");
 		}
 		else
 		{
-			Systtem.out.println("All the integers are not equal");
+			System.out.println("All the integers are not equal");
 		}
 		
 	}
