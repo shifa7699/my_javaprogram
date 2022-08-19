95aea30652a41375ee6e36e6e08d0b3c885e7e9d
shifa7699 <111556272+shifa7699@users.noreply.github.com>
2022-08-19 13:33:22 +0530

    Update even_odd.java

--- even_odd.java
+++ even_odd.java
@@ -5,6 +5,6 @@ public class even_odd
 	public static void main(String args[])
 	{
 		int num=12;
-		if(num%2==0)?Systtem.out.println("Even number"):Systtem.out.println("Odd number");
+		if(num%2==0)?System.out.println("Even number"):System.out.println("Odd number");
 	}
 }
