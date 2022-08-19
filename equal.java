3930b5b0339f3c35b7f6470e5ab159d2f3b20ffa
shifa7699 <111556272+shifa7699@users.noreply.github.com>
2022-08-19 13:05:36 +0530

    Add files via upload

---
+++ equal.java
@@ -0,0 +1,19 @@
+package program.com;
+
+public class equal 
+{
+	public static void main(String args[])
+	{
+		int a=6,b=6,c=6,d=6;
+		if(a==b && b==c && c==d)
+		{
+			Systtem.out.println("All the integers are equal");
+		}
+		else
+		{
+			Systtem.out.println("All the integers are not equal");
+		}
+		
+	}
+
+}
---
+++ leap_year.java
@@ -0,0 +1,18 @@
+package program.com;
+
+public class leap_year 
+{
+	public static void main(String args[])
+	{
+		int year=2016;
+		if(year%4==0)
+		{
+			Systtem.out.println(year+"is a leap year");
+		}
+		else
+		{
+			Systtem.out.println(year+"is not a leap year");
+		}
+	}
+
+}
