d1ffe1347ae7101f3e8d458dbf04bb53f333d063
shifa7699 <111556272+shifa7699@users.noreply.github.com>
2022-08-22 12:37:17 +0530

    Add files via upload

--- sum_n_numbers.java
+++ sum_n_numbers.java
@@ -6,12 +6,12 @@ public class sum_n_numbers
 	public static void main(String args[])
 	{
 		int n,i,s=0;
-		Scanner sc=new Scanner(System.in);
-        System.out.println("enter the numbers");
-        int n=sc.nextInt();
-        for(i=1;i<=n;i++)
+        for(i=1;i<=10;i++)
         {
-        	s=s+i;
+        	Scanner sc=new Scanner(System.in);
+            System.out.println("enter the numbers");
+            int n=sc.nextInt();
+        	s=s+n;
         }
         System.out.println("Sum is: "+s);
 	}
