825d00b1c3eea70bbfa86e34dfabc40f5dfad92c
shifa7699 <111556272+shifa7699@users.noreply.github.com>
2022-08-23 13:50:51 +0530

    Add files via upload

---
+++ multi_table.java
@@ -0,0 +1,18 @@
+package program.com;
+
+
+public class multi_table 
+{
+	  public static void main(String args[])
+	    {
+	        int i,j;
+	        for(i=1;i<=10;i++)
+	        {
+	            for(j=1;j<=10;j++)
+	            {
+	                System.out.print("\t"+i*j);
+	            }
+	             System.out.println();
+	        }
+	    }
+	}
---
+++ prime.java
@@ -0,0 +1,24 @@
+package program.com;
+
+
+public class prime 
+{
+	public static void main(String args[])
+    {
+         int a=15,b=30,i,j;
+         for(i=a; i<=b; i++)
+         {
+            for(j=2; j<=i ; j++)
+            {
+                if(i%j==0)
+                {
+                    break;
+                }
+            }
+          if(i==j)
+          {
+              System.out.println(j);
+          }
+        }
+    }
+}
