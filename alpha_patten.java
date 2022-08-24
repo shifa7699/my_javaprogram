9d28a24fb603283e18cefa4b0da1bd6e75140376
shifa7699 <111556272+shifa7699@users.noreply.github.com>
2022-08-24 11:33:54 +0530

    Add files via upload

---
+++ alpha_patten.java
@@ -0,0 +1,19 @@
+package program.com;
+
+
+public class alpha_patten 
+{
+	 public static void main(String args[])
+	    {
+	        int i,j;
+	        for(i=1;i<=4;i++)
+	        {
+	            for(j=1;j<=i;j++)
+	            {
+	                System.out.print((char)(j+64));
+	            }
+	            System.out.println();
+	        }
+	    }
+	}
+
