a2e8072a50a31b085ccad036092041ad09be001f
shifa7699 <111556272+shifa7699@users.noreply.github.com>
2022-08-25 18:07:27 +0530

    Update and rename marks.java to 19.08.2022/marks.java

--- 19.08.2022/marks.java
+++ 19.08.2022/marks.java
@@ -38,7 +38,16 @@ public class marks {
 		{
 			System.out.println("FAIL");
 		}
-		
 	}
-
 }
+/*Write a Java Program to take marks of a student and then print his/her 
+grade depending on the following conditions:
+
+90 to 100 then grade is A+, 
+80 to 90  then grade is A
+70 to 80 then grade is B+
+60 to 70 then grade is B
+50 to 60 then grade is C
+40 to 50 then grade is D
+30 to 40 then grade is E
+less than 30 then grade is F*/
