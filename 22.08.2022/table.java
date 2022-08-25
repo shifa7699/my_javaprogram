503f327f08e956e2e31654dfca0098a5896a1091
shifa7699 <111556272+shifa7699@users.noreply.github.com>
2022-08-25 17:52:51 +0530

    Update and rename table.java to 22.08.2022/table.java

---
+++ 22.08.2022/table.java
@@ -0,0 +1,18 @@
+package program.com;
+
+import java.util.Scanner;
+public class table 
+{
+	public static void main(String args[])
+	{
+		int n,i;
+		Scanner sc=new Scanner(System.in);
+       	 	System.out.println("enter the numbers");
+        	int n=sc.nextInt();
+        	for(i=1;i<=10;i++)
+        	{
+        		System.out.println(n*i);
+        	}
+      	}
+}
+//Write a program in Java to display the multiplication table of a given integer.
