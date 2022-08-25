df4f9f6a97cb6498ef523dbe6e01ac63c35400d1
shifa7699 <111556272+shifa7699@users.noreply.github.com>
2022-08-25 18:15:35 +0530

    Update leap_year.java

--- 19.08.2022/leap_year.java
+++ 19.08.2022/leap_year.java
@@ -15,4 +15,8 @@ public class leap_year
 		}
 	}
 }
-//Write a Java program that takes a year from user and print whether that year is a leap year or not.
+/*Write a Java program that takes a year from user and print whether that year is a leap year or not.
+Test Data
+Input the year: 2016
+Expected Output :
+2016 is a leap year*/
