b760ff043ee4b33bbd8a5828047982a4e387c4b5
shifa7699 <111556272+shifa7699@users.noreply.github.com>
2022-08-29 11:56:40 +0530

    Update and rename inherit.java to 29.08.2022/InheritanceFolder/inherit.java

---
+++ 29.08.2022/InheritanceFolder/inherit.java
@@ -0,0 +1,16 @@
+package program.com;
+
+import java.util.*;
+public class inherit 
+{
+	public static void main(String args[])
+	 {
+	      square s=new square();
+	      s.display();
+	      s.area_square();
+	  }
+}
+/*Create a class called Shape which has a property side.
+Create a subclass of Shape called Square which should use the property of 
+Shape class to display area of square
+Note: Take input*/
