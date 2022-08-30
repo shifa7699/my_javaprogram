4c19ea2051527834068e6f3e4e0a6391eac1840e
shifa7699 <111556272+shifa7699@users.noreply.github.com>
2022-08-30 11:44:15 +0530

    Add files via upload

---
+++ assign.java
@@ -0,0 +1,15 @@
+package program.com;
+
+
+public class assign 
+{
+	public static void main(String[] args)
+    {        
+        Rectangle r = new Rectangle(4,5);   
+        Square s = new Square(4);      
+        r.print_area();       
+        r.print_perimeter();  
+        s.print_area();      
+        s.print_perimeter();    
+    }
+}
