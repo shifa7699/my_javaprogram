2ddda3b4b661dfb63f2da81c8111135871604ae1
shifa7699 <111556272+shifa7699@users.noreply.github.com>
2022-09-01 13:11:38 +0530

    Add files via upload

---
+++ array_1.java
@@ -0,0 +1,18 @@
+package program.com;
+
+import java.util.*;
+public class array_1 
+{
+	public static void main(String args[])
+	{
+	      int i;
+	      String arr[];
+	      Scanner sc=new Scanner(System.in);
+	      System.out.println("enter the array");
+	      arr=sc.nextLine();
+	      for(i=0;i<arr.length;i++)
+	      {
+	    	  System.out.println(arr[i]);
+		  }
+	 }
+}
