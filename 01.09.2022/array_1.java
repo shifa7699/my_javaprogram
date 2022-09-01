946d47d8b0cb60a828989c235c7fff8f951a913d
shifa7699 <111556272+shifa7699@users.noreply.github.com>
2022-09-01 13:55:47 +0530

    Update array_1.java

--- 01.09.2022/array_1.java
+++ 01.09.2022/array_1.java
@@ -7,12 +7,15 @@ public class array_1
 	{
 	      int i;
 	      String arr[]=new String arr[5];
-	      Scanner sc=new Scanner(System.in);
-	      System.out.println("Enter the array");
-	      arr=sc.nextLine();
 	      for(i=0;i<arr.length;i++)
 	      {
-	    	  System.out.println(arr[i]);
+		     	Scanner sc=new Scanner(System.in);
+	     	 	System.out.println("Enter the array");
+	      		arr=sc.nextLine();
+		}
+		for(i=0;i<arr.lenght;i++)
+		{
+			System.out.println(arr[i]);
 		}
 	 }
 }
