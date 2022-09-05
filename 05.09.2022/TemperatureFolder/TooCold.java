d0bcc82c7fde88218455c69ccb4e19093df746ae
shifa7699 <111556272+shifa7699@users.noreply.github.com>
2022-09-05 13:03:36 +0530

    Add files via upload

---
+++ 05.09.2022/TemperatureFolder/TooCold.java
@@ -0,0 +1,11 @@
+package program.com;
+
+
+
+public class TooCold extends Exception
+{
+	public TooCold(String msg) 
+	{
+		super(msg);
+	}
+}
---
+++ 05.09.2022/TemperatureFolder/TooHot.java
@@ -0,0 +1,11 @@
+package pogram.com;
+
+
+public class TooHot extends Exception
+{
+    public TooHot(String msg)
+    {
+        super(msg);
+    }
+}
+
