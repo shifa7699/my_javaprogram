53eb8ac35ae428e07604f18c54045e03c8979774
shifa7699 <111556272+shifa7699@users.noreply.github.com>
2022-09-07 12:45:37 +0530

    Update test_1.java

--- 07.09.2022/TestFolder/test_1.java
+++ 07.09.2022/TestFolder/test_1.java
@@ -6,14 +6,14 @@ import org.junit.jupiter.api.Test;
 
 class test_1 
 {
-    @Test
-	  void test() 
-    {
-		    Max ob=new Max();
-		    int arr[]= {90,80,40,20};
-		    int actual=ob.maximum(arr);
-		    int expected=90;
-		    assertEquals(expected,actual);
-		}
+    	@Test
+	void test() 
+    	{
+		 Max ob=new Max();
+		 int arr[]= {90,80,40,20};
+		 int actual=ob.maximum(arr);
+		 int expected=90;
+		 assertEquals(expected,actual);
+	}
 }
 //Create a junit test case for finding the maximum value in an array
